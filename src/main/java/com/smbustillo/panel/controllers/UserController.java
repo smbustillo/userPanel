4e171060ce54e8fff64188f7096e793008e4eefa
Susana M. Bustillo <smbustill@gmail.com>
2023-07-18 19:31:31 +0200

    first commit

--- src/main/java/com/smbustillo/panel/controllers/UserController.java
+++ src/main/java/com/smbustillo/panel/controllers/UserController.java
@@ -1,4 +1,28 @@
 package com.smbustillo.panel.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping("/api/usuarios")
+    public String helloWorld() {
+        return "[\n" +
+                "  {\n" +
+                "    \"id\": 1,\n" +
+                "    \"nombre\": \"lucas\",\n" +
+                "    \"apellido\": \"moy\",\n" +
+                "    \"telefono\":\"213123423\",\n" +
+                "    \"email\": \"lucasmoy.dev@atl.academy\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"id\": 1,\n" +
+                "    \"nombre\": \"lucas\",\n" +
+                "    \"apellido\": \"moy\",\n" +
+                "    \"telefono\":\"213123423\",\n" +
+                "    \"email\": \"lucasmoy.dev@atl.academy\"\n" +
+                "  }\n" +
+                "]";
+    }
 }
